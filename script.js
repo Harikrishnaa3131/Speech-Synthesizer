337520104b55fa9ee9551c3cc44ae9943ec32ade
Harikrishnaa3131 <52791819+Harikrishnaa3131@users.noreply.github.com>
2021-09-23 17:56:56 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,43 @@
+const msg = new SpeechSynthesisUtterance();
+let voices = [];
+const voicesDropdown = document.querySelector('[name="voice"]');
+const options = document.querySelectorAll('[type="range"], [name="text"]');
+const speakButton = document.querySelector('#speak');
+const stopButton = document.querySelector('#stop');
+msg.text = document.querySelector('[name = "text"]').value;
+
+function populateVoices() {
+  voices = this.getVoices(); // array of 25 synthesised voices, including name & language
+  voicesDropdown.innerHTML = voices
+  .filter(voice => voice.lang.includes('en')) // limit 25 voices to just the ones in English
+  .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`)
+  .join('');
+}
+
+//set name of voice to the same as the option chosen.
+function setVoice() {
+  msg.voice = voices.find(voice => voice.name === this.value);
+  toggle();
+}
+
+/*The cancel() method of the SpeechSynthesis interface removes all utterances from 
+the utterance queue.*/
+function toggle(startOver = true) {
+  speechSynthesis.cancel();
+  if(startOver) {
+    speechSynthesis.speak(msg);
+  }
+}
+
+function setOption() {
+  console.log(this.name, this.value); // show rate and pitch values
+  msg[this.name] = this.value;
+}
+
+speechSynthesis.addEventListener('voiceschanged', populateVoices);
+voicesDropdown.addEventListener('change', setVoice);
+
+
+options.forEach(option => option.addEventListener('change', setOption));
+speakButton.addEventListener('click', toggle);
+stopButton.addEventListener('click', () => toggle(false));
